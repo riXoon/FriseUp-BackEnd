68e0b1472a2fe870556de7a1f2e2bd7b05e44988
Erickson <rixon.code@gmail.com>
2024-12-17 10:46:23 +0800

    database connection

---
+++ db_connect.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	* Database Connection
+	*/
+	class db_connect {
+		private $server = 'localhost';
+		private $dbname = 'dbfriseup';
+		private $user = 'root';
+		private $pass = '';
+
+		public function connect() {
+			try {
+				$conn = new PDO('mysql:host=' .$this->server .';dbname=' . $this->dbname, $this->user, $this->pass);
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				return $conn;
+			} catch (\Exception $e) {
+				echo "Database Error: " . $e->getMessage();
+			}
+		}
+	}
+?>
